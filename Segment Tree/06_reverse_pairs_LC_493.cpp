4139a89208f41ef6a3fe309a99f6e74aa2fa9a9b
ama29n <ama3149n@gmail.com>
2022-10-05 23:39:36 +0530

    5 October 2022

--- Segment Tree/06_reverse_pairs_LC_493.cpp
+++ Segment Tree/06_reverse_pairs_LC_493.cpp
@@ -6,57 +6,6 @@ using namespace std;
 
 // Find pairs such that nums[i] > 2 * nums[j] where i < j
 
-// Using merge sort
-class Solution {
-public:
-    void merge(vector<int>& arr, int l, int mid, int r) {
-        vector<int> nums(r - l + 1);
-        int i = l, j = mid + 1, k = 0;
-        while(k < nums.size()) {
-            int num1 = i > mid ? INT_MAX : arr[i];
-            int num2 = j > r ? INT_MAX : arr[j];
-            nums[k++] = num1 <= num2 ? arr[i++] : arr[j++];
-        }
-        for(int idx = 0; idx < nums.size(); idx++)
-            arr[l + idx] = nums[idx];
-    }
-    int mergeSort(vector<int>& nums, int l, int r) {
-        if(l == r)
-            return 0;
-        int mid = l + (r - l) / 2;
-        int count = mergeSort(nums, l, mid) + mergeSort(nums, mid + 1, r);
-        int i = l, j = mid + 1;
-        while(i <= mid && j <= r) {
-            if(nums[i] > (long)nums[j] * 2)
-                count += mid - i + 1, j++;
-            else
-                i++;
-        }
-        merge(nums, l, mid , r);
-        return count;
-    }
-    int reversePairs(vector<int>& nums) {
-        return mergeSort(nums, 0, nums.size() -  1);
-    }
-};
-
-// Multiset Solution, gives TLE
-class Solution {
-public:
-    typedef long long ll;
-    int reversePairs(vector<int>& nums) {
-        int ans = 0;
-        multiset<ll> s;
-        for(auto it : nums) {
-            auto iter = s.upper_bound((ll)it * 2);
-            int dis = distance(iter, s.end());
-            ans += dis;
-            s.insert((ll)it);
-        }
-        return ans;
-    }
-};
-
 // Segment Tree Solution
 class Solution {
 public:
