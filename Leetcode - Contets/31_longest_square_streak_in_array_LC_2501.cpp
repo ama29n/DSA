7630cd83fc069e41262e6204bd8ac04966be084a
ama29n <ama3149n@gmail.com>
2023-01-26 16:32:31 +0530

    26 January 2023

--- Leetcode - Contets/31_longest_square_streak_in_array_LC_2501.cpp
+++ Leetcode - Contets/31_longest_square_streak_in_array_LC_2501.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 // Square streak is a subsequence such that after sorting, all elements are square of previous element, except the first one
 
+// DP pattern of longest sequence but here there is a relation between elements, thus map can be used
 class Solution {
 public:
     typedef long long ll;
@@ -13,7 +14,9 @@ public:
         sort(nums.begin(), nums.end());
         reverse(nums.begin(), nums.end());
         unordered_map<int, int> map;
+        // Start from big to small
         for(int i = 0; i < n; i++) {
+            // If square is not found then, max sequence = 1
             if(map.find((ll)nums[i] * (ll)nums[i]) == map.end()) {
                 map[nums[i]] = 1;
             } else {
